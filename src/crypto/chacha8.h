a16bd845807f3432b60685db590fe04c6e88717c
ZedPea <zp+github@zacharypalmer.co.uk>
2018-07-12 00:01:30 +0100

    Update licenses

--- src/crypto/chacha8.h
+++ src/crypto/chacha8.h
@@ -1,4 +1,8 @@
-#pragma once
+// Copyright (c) 2012-2017, The CryptoNote developers, The Bytecoin developers
+// Copyright (c) 2014-2018, The Monero Project
+// Copyright (c) 2018, The TurtleCoin Developers
+// 
+// Please see the included LICENSE file for more information.#pragma once
 
 #include <stdint.h>
 #include <stddef.h>
--- src/crypto/slow-hash.c
+++ src/crypto/slow-hash.c
@@ -1,4 +1,5 @@
 // Copyright (c) 2014-2018, The Monero Project
+// Copyright (c) 2014-2018, The Aeon Project
 //
 // All rights reserved.
 //
