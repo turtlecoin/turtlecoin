cdb2d0955c0e3318687cce139d58d2d004099e2f
Franzferdinan51 <Franzferdinan51@gmail.com>
2018-11-24 10:10:22 -0500

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -10,10 +10,10 @@
 namespace WalletConfig
 {
     /* The prefix your coins address starts with */
-    const std::string addressPrefix = "GRUB";
+    const std::string addressPrefix = "MagiX";
 
     /* Your coins 'Ticker', e.g. Monero = XMR, Bitcoin = BTC */
-    const std::string ticker = "GRUB";
+    const std::string ticker = "MAGI";
 
     /* The filename to output the CSV to in save_csv */
     const std::string csvFilename = "transactions.csv";
@@ -23,14 +23,14 @@ namespace WalletConfig
     const std::string addressBookFilename = ".addressBook.json";
 
     /* The name of your deamon */
-    const std::string daemonName = "GrubMined";
+    const std::string daemonName = "Magnusd";
 
     /* The name to call this wallet */
-    const std::string walletName = "Grubwallet";
+    const std::string walletName = "Magiwallet";
 
     /* The name of service/walletd, the programmatic rpc interface to a
        wallet */
-    const std::string walletdName = "Grub-service";
+    const std::string walletdName = "Magi-service";
 
     /* The full name of your crypto */
     const std::string coinName = std::string(CryptoNote::CRYPTONOTE_NAME);
