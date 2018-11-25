e7c86b800a23e3bcc66648c4e9b6f835f7d73bea
Franzferdinan51 <Franzferdinan51@gmail.com>
2018-11-25 16:47:27 -0500

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -26,7 +26,7 @@ namespace WalletConfig
     const std::string daemonName = "Magnusd";
 
     /* The name to call this wallet */
-    const std::string walletName = "MagiXwallet";
+    const std::string walletName = "Magiwallet";
 
     /* The name of service/walletd, the programmatic rpc interface to a
        wallet */
