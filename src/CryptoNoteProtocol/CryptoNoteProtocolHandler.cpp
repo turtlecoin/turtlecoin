b14cd8bbe9dbc35a0969cddee8ee8758d601060b
Morpheus <43630833+19morpheus80@users.noreply.github.com>
2018-12-21 01:46:48 +0000

    Update CryptoNoteProtocolHandler.cpp
    Fixed in other branches but not this one; replaced 'TurtleCoin' with " << CryptoNote::CRYPTONOTE_NAME << "

--- src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
+++ src/CryptoNoteProtocol/CryptoNoteProtocolHandler.cpp
@@ -605,7 +605,7 @@ bool CryptoNoteProtocolHandler::request_missing_objects(CryptoNoteConnectionCont
     requestMissingPoolTransactions(context);
 
     context.m_state = CryptoNoteConnectionContext::state_normal;
-    logger(Logging::INFO, Logging::BRIGHT_GREEN) << context << "Successfully synchronized with the TurtleCoin Network.";
+    logger(Logging::INFO, Logging::BRIGHT_GREEN) << context << "Successfully synchronized with the " << CryptoNote::CRYPTONOTE_NAME << " network.";
     on_connection_synchronized();
   }
   return true;
