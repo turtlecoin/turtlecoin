cc793f372d4489a92640e32624b7a872977efbb6
Nick <44332091+Xstream1@users.noreply.github.com>
2018-12-27 01:47:22 -0800

    Update WalletConfig.h

--- src/config/WalletConfig.h
+++ src/config/WalletConfig.h
@@ -23,7 +23,7 @@ namespace WalletConfig
     const std::string addressBookFilename = ".addressBook.json";
 
     /* The name of your deamon */
-    const std::string daemonName = "Galaxybitzd";
+    const std::string daemonName = "GalaxyBitzd";
 
     /* The name to call this wallet */
     const std::string walletName = "zedwallet";
